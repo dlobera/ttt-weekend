904fd1eeb09f08c50195de0ddd6cec64dc41a308
dlobera <delobera@gmail.com>
2022-06-02 14:39:18 -0500

    add function

--- js/app.js
+++ js/app.js
@@ -3,11 +3,15 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let board, turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelectorAll('square')
+console.log('.square')
 
+const messageEl = document.querySelector('h2')
+console.log(messageEl)
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -15,4 +19,8 @@
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
+function init() {
+console.log('sanity check')
+}
 
