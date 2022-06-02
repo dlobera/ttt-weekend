b1deeabec539d5ccfa5c5156204bdce5c5348220
dlobera <delobera@gmail.com>
2022-06-02 17:57:45 -0500

    add message to render

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
-const players
+let winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -7,8 +16,8 @@ let board, turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
-const squareEls = document.querySelectorAll('square')
-console.log('.square')
+const squareEls = document.querySelectorAll('.square')
+console.log(squareEls)
 
 const messageEl = document.querySelector('h2')
 console.log(messageEl)
@@ -21,12 +30,32 @@ console.log(messageEl)
 /*-------------------------------- Functions --------------------------------*/
 init()
 function init() {
-  board = [null, null, null, null, null, null, null, null, null]
+  board = [1, null, null, null, null, null, null, null, null]
   turn = 1 // represents player x
   winner = null // null = no winner, 1 = x won, -1 = o won, t= tie, anything else game over
   render() 
 }
 
 function render () {
-  
+  board.forEach(function(square, i) {
+    console.log(squareEls[i])
+    if (square === 1) {
+      squareEls[i].textContent = 'X' 
+    } 
+    if (square === -1) {
+      squareEls[i].textContent = 'O'
+    }
+  })
+  renderMessage()
 }
+
+function renderMessage() {
+  if (winner === null) {
+    messageEl.textContent = `Player ${turn === 1 ? 'X' : 'O'}'s turn!`
+  } else if (winner === 'T') {
+    messageEl.textContent = `It's a tie!`
+  } else {
+    messageEl.textContent = `Player ${turn === 1 ? 'O' : 'X'} won!`
+  }
+}
+
